0b095a3a571b42f6357339162cb1442792085304
Enrique Coello <enrique.coello@gmail.com>
2019-10-15 21:04:56 -0400

    Working solution for ransom problem using ArrayList

--- src/practice/problems/dictionariesandhashmaps/HashTablesRansomNote.java
+++ src/practice/problems/dictionariesandhashmaps/HashTablesRansomNote.java
@@ -1,26 +1,76 @@
 package practice.problems.dictionariesandhashmaps;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HashTablesRansomNote {
-    // Complete the checkMagazine function below.
     static void checkMagazine(String[] magazine, String[] note) {
-        List<String> magazineList = new ArrayList<>();
-        List<String> noteList = new ArrayList<>();
         boolean match = true;
+        long noteKey = 0;
+        long magazineKey = 0;
 
+//        Map<Long, String> magazineList = new ConcurrentHashMap<>();
+//        for (String s : magazine) {
+//            magazineList.put(magazineKey, s);
+//            magazineKey++;
+//        }
+//
+//        Map<Long, String> noteList = new HashMap<>();
+//        for (String s : note) {
+//            noteList.put(noteKey, s);
+//            noteKey++;
+//        }
+
+        // loop through notes values
+        // if note value is found in magazine value
+        // get magazine key for this value
+        // remove magazine entry with key and value
+        // else
+        // set match to false
+
+//        for (String n : noteList.values()) {
+//            System.out.println("\nNOTE WORD: " + n);
+//            for (Map.Entry<Long, String> mapEntry : magazineList.entrySet()) {
+////                System.out.println("MAGAZINE KEY: " + mapEntry.getKey() + ", MAGAZINE VALUE: " + mapEntry.getValue());
+//                if (!n.equals(mapEntry.getValue())) {
+////                    System.out.println("NOTE WORD NOT FOUND IN MAGAZINE LIST: " + mapEntry.getValue());
+//                    match = false;
+//                    break;
+//                } else {
+//                    System.out.println("NOTE WORD FOUND IN MAGAZINE LIST: " + mapEntry.getValue());
+//                    magazineList.remove(mapEntry.getKey());
+//                }
+//            }
+//        }
+
+
+//        mapEntry = magazineList.entrySet();
+//        System.out.println("key: " + mapEntry.getKey() + ", value: " + mapEntry.getValue());
+
+//        Set<Map.Entry<Long, String>> setEntryLongString = magazineList.entrySet();
+//        for (Map.Entry<Long, String> entry: setEntryLongString)
+//        {
+//            System.out.println("key: " + entry.getKey() + ", value: " + entry.getValue());
+//            if ("three".equals(entry.getValue()))
+//            {
+//                System.out.println("found three");
+//            }
+//            else
+//            {
+//                System.out.println("did not find three");
+//            }
+//        }
+
+
+//        WORKS WITH ARRAYLIST!
+        List<String> magazineList = new ArrayList<>();
         for (String s : magazine)
             magazineList.add(s);
-
-        for (String s : note)
-            noteList.add(s);
-
-        for (String n : noteList) {
-            if (!magazineList.contains(n))
+        for (String n : note) {
+            if (!magazineList.contains(n)) {
                 match = false;
-            else {
+                break;
+            } else {
                 magazineList.remove(n);
             }
         }
@@ -52,17 +102,17 @@ public class HashTablesRansomNote {
 //            note[i] = noteItem;
 //        }
 
-        // GOOD ONE:
+        // YES
 //        String[] magazine = {"give", "me", "one", "grand", "today", "night"};
 //        String[] note = {"give", "one", "grand", "today"};
 
-        // GOOD OR BAD
-        String[] magazine = {"ive", "got", "a", "lovely", "bunch", "of", "coconuts"};
-        String[] note = {"ive", "got", "some", "coconuts"};
+        // NO
+//        String[] magazine = {"ive", "got", "a", "lovely", "bunch", "of", "coconuts"};
+//        String[] note = {"ive", "got", "some", "coconuts"};
 
-        // GOOD OR BAD
-//        String[] magazine = {""};
-//        String[] note = {""};
+        // NO
+        String[] magazine = {"two", "times", "three", "is", "not", "four"};
+        String[] note = {"two", "times", "two", "is", "four"};
 
         checkMagazine(magazine, note);
         scanner.close();
